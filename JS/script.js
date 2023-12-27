8c8616f74c9112dfe5edca219c64a256dd21e503
Dat_muslimguy <azeezmubarak11@gmail.com>
2023-12-27 21:09:44 +0100

    updated

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,24 @@
+const audio = document.getElementById('background-audio');
+
+audio.muted = true;
+
+audio.addEventListener('contextmenu', (e) => (
+    e.preventDefault()
+));
+
+audio.controls = false;
+audio.addEventListener('playing', () => {
+    audio.controls = false
+});
+
+audio.addEventListener('ended', function() {
+    audio.play();
+})
+
+setTimeout(function() {
+    audio.play();
+} , 5000);
+
 document.addEventListener('DOMContentLoaded', () => {
     const images = document.querySelectorAll('.lazy-img');
 
